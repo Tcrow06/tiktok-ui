169cd29195bf6556022a26fc447c0ce96e51d0ca
Tcrow06 <tcrow0604@gmail.com>
2025-03-23 23:44:54 +0700

    config routes/layout

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+// Public routes
+// Cho phép truy cập khi không cần login
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Private routes
+// Nếu chưa đăng nhập thì chuyển sang trang đăng nhập
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };

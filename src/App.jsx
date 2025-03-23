5179f68ac9b89514187d93f1a34dc1c6b8cd9bb7
Tcrow06 <tcrow0604@gmail.com>
2025-03-23 22:47:49 +0700

    add use css, sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+
 function App() {
-  return (
-    <div className="bg-red-600">app</div>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/following" element={<Following />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,12 @@ export default defineConfig({
   },
   preview: {
     port: 8080
-  }
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"), 
+    },
+  },
   
 })
 

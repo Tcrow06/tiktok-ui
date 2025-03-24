412bf410d12cd2080e9f5b909f895ab726124840
Tcrow06 <tcrow0604@gmail.com>
2025-03-24 11:19:48 +0700

    Build DefaultLayout frame

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,5 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+//dùng classnames.blind để có thể dùng tên class post-item
+// thay vì postItem
+const cx = classNames.bind(styles);
+console.log(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                
+            </div>
+        </header>
+    );
 }
 
 export default Header;
